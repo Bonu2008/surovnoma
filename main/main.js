d93e2cd904b2ff90b4a64bdee8c221025914858a
Bonu2008 <mehribonurashidova2008>
2023-11-11 22:55:35 +0500

    finsh

---
+++ main/main.js
@@ -0,0 +1,44 @@
+var Salom = String(prompt("Assalomu aliykum😊"))
+console.log(Salom);
+
+var Ism = String(prompt("Ismingiz nima🤔"))
+console.log(Ism);
+
+var Hobbiy = String(prompt("Nima narsaniga qiziqasiz? Ya'ni hobbiyingiz nima🤗"))
+console.log(Hobbiy);
+
+var Kasb = String(prompt("O'z kasbingizdan faxrlanasizmi🧑‍💻"))
+console.log(Kasb);
+
+var Kitob = String(prompt("Eng yaxshi ko'rib o'qigan kitobingiz nomi?📚"))
+console.log(Kitob);
+
+var Yomon = String(prompt("Kursimizdagi eng yomon ko'gan o'qivchingiz bormi  (ha,yo'q)"))
+console.log(Yomon);
+
+var Orzu = String(prompt("Eng katta orzuingiz"))
+console.log(Orzu);
+
+var Men = String(prompt("Men haqimda qanaqa fikrdasiz"))
+console.log(Men);
+
+var KunlikHarajat = Number(prompt("Siz bir kunda qancha pul harajat qilasiz?"))
+console.log(KunlikHarajat);
+
+var Savol = String(prompt("Sizningcha hamma insonlar bir xilmi pisixologik jihatdan"))
+console.log(Savol);
+
+var Javob = [
+    "Salom😊: "+" "+ Salom +"\n" +
+    "Ismingiz🤔: "+" "+ Ism +"\n" +
+    "Hobbiyingiz🤗: "+" "+ Hobbiy +"\n" +
+    "Faxirlanasizmi🧑‍💻: "+" "+ Kasb +"\n" +
+    "Kitob📚: "+" "+ Kitob +"\n" +
+    "Eng yomon o'quvchi: "+" "+ Yomon +"\n" +
+    "Orzuingiz: "+" "+ Orzu +"\n" +
+    "Fikringiz: "+" "+ Men +"\n" +
+    "Kunlik Harajatingiz: "+" "+ KunlikHarajat +"\n" +
+    "Savol: "+" "+ Savol +"\n"
+]
+
+alert (Javob)
